49c648a76ef6d4ece0f3b4edf30b2258e47cdbe3
= <=>
2022-05-14 12:54:43 +0530

    updated changes

--- Jquery-tutorials/script.js
+++ Jquery-tutorials/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
-$("button").click(function(){
+
+  // $("p").click(function(){
+  //   $(this).hide();
+  // });
+
+  // $("p").on("click", function(){
+  //   $(this).hide();
+  // }); 
+
+  // $("#p1").mouseenter(function(){
+  //   alert("You entered p1!");
+  // })
+
+  // $("#p1").mouseleave(function(){
+  //   alert("hello ajaml!");
+  // })
+
+  // $("#p1").mousedown(function(){
+  //   alert("Mouse down over p1!");
+  // });
+
+  // $("#p1").mouseup(function(){
+  //   alert("Mouse up over p1!");
+  // });
+  
+  // $(".p1").hover(function(){
+  //   alert("You entered p1!");
+  // });
+  
+  // $(".p1").hover(function(){
+  //   alert("You entered p1!");
+  // },
+  // function(){
+  //   alert("Bye! You now leave p1!");
+  // });
+  
+  // $(".p1").hover(function(){
+  //   $(this).css("background-color", "#cccccc");
+  // });
+
+  // $("p").hide();
+
+  $("#show").click(function(){
     $("p").toggle();
   });
+  
+  
+  
+
 });
